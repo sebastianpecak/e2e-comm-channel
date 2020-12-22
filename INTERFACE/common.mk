b846793d00bbe19663badd19f3a5d8e58319a42b
Sebastian Pecak <sebastianpecak@gmail.com>
2020-12-22 21:35:02 +0100

    Implemented interface library.

--- INTERFACE/common.mk
+++ INTERFACE/common.mk
@@ -1,4 +1,13 @@
 # Common project's tools.
 
-RM = rm -rf
-PBC = protoc
+RM = `which rm` -rf
+PBC = `which protoc`
+C = `which gcc`
+CC = `which g++`
+AR = `which ar` rcs
+
+ALL_PROTO = $(shell ls *.proto 2>/dev/null)
+BUILD_PROTO = $(filter-out $(EXCLUDED_PROTO), $(ALL_PROTO))
+BUILD_SOURCES = $(patsubst %.proto,%.pb.cc,$(BUILD_PROTO))
+BUILD_HEADERS = $(patsubst %.proto,%.pb.h,$(BUILD_PROTO))
+BUILD_OBJECTS = $(patsubst %.proto,%.pb.cc.o,$(BUILD_PROTO))
